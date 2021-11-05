2541a997da7b290750ef51d4f02cc885df60028c
ckswns <jyp3515@naver.com>
2021-11-05 13:44:45 +0900

    Editor Camera Update
    속도값을 설정할수 있도록 변경

--- DirectX_SR/Engine/GameSystem/EditorCamera.h
+++ DirectX_SR/Engine/GameSystem/EditorCamera.h
@@ -8,12 +8,13 @@ namespace ce
 
 	class EditorCamera : public Behaviour
 	{
-	public:		explicit	EditorCamera(HWND hWnd) noexcept;
+	public:		explicit	EditorCamera(HWND hWnd, float velocity = 0.5f) noexcept;
 	public:		virtual		~EditorCamera(void) noexcept { __noop; }
 
 	public:		void		Start(void) noexcept override;
 	public:		void		Update(float fElapsedTime) noexcept override;
 
+	private:	float		_velocity;
 	private:	bool		_bMBRightDown = false;
 	private:	bool		_bWireFrame = false;
 	private:	POINT		_ptPrevMousePos;
