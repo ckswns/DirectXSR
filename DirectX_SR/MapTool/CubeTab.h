f2d1b334703375e944f513da39af8c13b27a6f8c
Alen <chaicofski@gmail.com>
2021-11-05 16:32:28 +0900

    55

--- DirectX_SR/MapTool/CubeTab.h
+++ DirectX_SR/MapTool/CubeTab.h
@@ -22,4 +22,19 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedCreateCube();
+	afx_msg void OnBnClickedDataSave();
+	afx_msg void OnBnClickedDataLoad();
+	float _fScaleX;
+	float _fScaleY;
+	float _fScaleZ;
+	afx_msg void OnBnClickedScaleApply();
+
+	GameObject* _pGameObject;
+	std::vector<GameObject*>	_vecObject;
+
+	afx_msg void OnBnClickedScaleReset();
+	CListBox _SaveList;
+	uint8	_CubeNumber;
+	CListBox _TextureList;
+	afx_msg void OnBnClickedLoadTextureList();
 };
