b8f1711ab61b93244a897134f57a4bab62de36d9
ckswns <jyp3515@naver.com>
2021-11-16 15:41:47 +0900

    bug fix

--- DirectX_SR/Engine/Base/GameObject.cpp
+++ DirectX_SR/Engine/Base/GameObject.cpp
@@ -430,12 +430,12 @@ namespace ce
 				return vObjects_EFFECT[i];
 		}
 
-		const std::queue<GameObject*>& vWaitInsert = SceneManager::Instance()->GetActiveScene()->GetSceneWaitInsertGameObjectXXXXXXXXXXXX();
+		const std::vector<GameObject*>& vWaitInsert = SceneManager::Instance()->GetActiveScene()->GetSceneWaitInsertGameObjectXXXXXXXXXXXX();
 
 		for (int i = 0; i < vWaitInsert.size(); i++)
 		{
-			if (vWaitInsert._Get_container()[i]->GetTag() == tag)
-				return vWaitInsert._Get_container()[i];
+			if (vWaitInsert[i]->GetTag() == tag)
+				return vWaitInsert[i];
 		}
 
 		return nullptr;
