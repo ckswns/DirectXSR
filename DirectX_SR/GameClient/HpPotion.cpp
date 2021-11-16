06b20de1a4817214220db901348f20d2ddc47c0c
JSH <taraduncan206@gmail.com>
2021-11-16 17:33:48 +0900

    hp Potion cpp

--- DirectX_SR/GameClient/HpPotion.cpp
+++ DirectX_SR/GameClient/HpPotion.cpp
@@ -4,6 +4,7 @@
 #include "SpriteRenderer.h"
 #include "Animation.h"
 #include "Animator.h"
+#include "Transform.h"
 
 #include "Slot.h"
 HpPotion::HpPotion() noexcept
@@ -11,16 +12,17 @@ HpPotion::HpPotion() noexcept
 	_tInvenItem._eitemID = (int)ITEM_ID::HP;
 	_tInvenItem._eSlotType = (int)Slot::SLOTTYPE::POTION;
 	_tInvenItem._strName = "HP Potion";
-	_tInvenItem._strInvenImgPath = "";
+	_tInvenItem._strInvenImgPath = "Asset\\UI\\Inventory\\Hp";
 	_tInvenItem._iValue = 20;
 }
 
 void HpPotion::Start(void) noexcept
 {
 	gameObject->SetTag(GameObjectTag::OBJECT);
+	gameObject->SetName("Item");
 
-	gameObject->AddComponent(new BoxCollider(D3DXVECTOR3(0.3, 0.3f, 0.3f)));
-	SpriteRenderer* sr = new SpriteRenderer(D3D9DEVICE->GetDevice(), ASSETMANAGER->GetTextureData("Asset\\Player\\Player.png"));
+	gameObject->AddComponent(new BoxCollider(D3DXVECTOR3(0.3, 1, 0.3f)));
+	SpriteRenderer* sr = new SpriteRenderer(D3D9DEVICE->GetDevice(), ASSETMANAGER->GetTextureData("Asset\\Item\\HpPotion\\0.png"));
 	gameObject->AddComponent(sr);
 
 	Animator* pAnimator = new Animator(true);
@@ -36,7 +38,7 @@ void HpPotion::Start(void) noexcept
 		sprintf_s(str, 256, "Asset\\Item\\HpPotion\\%d.png", i);
 
 		TList.push_back(ASSETMANAGER->GetTextureData(str));
-		FrameTime.push_back(0.3f);
+		FrameTime.push_back(0.05f);
 	}
 
 	Animation* ani = new Animation(FrameTime, TList, false);
@@ -45,4 +47,6 @@ void HpPotion::Start(void) noexcept
 
 	TList.clear();
 	FrameTime.clear();
+
+	Item::Start();
 }
