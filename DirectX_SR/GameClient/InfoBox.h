cfe43ec4274e4b86f879771f3f88d23cab3659b5
JSH <taraduncan206@gmail.com>
2021-11-20 22:37:37 +0900

    StoreItem InfoBox 출력
    StoreItem InfoBox 출력
    Player 아이템 장착 시 스탯을 변경하는 함수( EquidItem() ) 추가

--- DirectX_SR/GameClient/InfoBox.h
+++ DirectX_SR/GameClient/InfoBox.h
@@ -12,7 +12,7 @@ namespace ce
 
 class InfoBox : public Behaviour
 {
-public: explicit InfoBox(Slot::SLOTTYPE eType);
+public: explicit InfoBox(Slot::SLOTTYPE eType, bool isStoreItem = false);
 public: virtual ~InfoBox();
 
 public:	virtual void	Awake(void) noexcept override;
@@ -22,12 +22,13 @@ public: virtual void	Update(float) noexcept override;
 public: void			SetPosition(float x, float y);
 public: void			ShowInfoBox(bool Check) { gameObject->SetActive(Check); }
 public: void			MissingItem();
+public: void			SetStoreItem(bool b);
 
 private: ITEMDATA			_data;
 private: UI::Text*			_pText;
 private: UI::Text*			_pTex[9];
 private: Transform*			_pPare;
 private: Slot::SLOTTYPE		_eType;
-
+private: bool				_bStore;
 };
 
