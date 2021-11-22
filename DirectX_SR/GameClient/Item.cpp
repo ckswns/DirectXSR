a8784118395654fe0af046f02be0e155c7bb4c1a
JSH <taraduncan206@gmail.com>
2021-11-22 13:08:18 +0900

    Update Item.cpp

--- DirectX_SR/GameClient/Item.cpp
+++ DirectX_SR/GameClient/Item.cpp
@@ -15,7 +15,7 @@ void Item::Start(void) noexcept
 	gameObject->SetTag(GameObjectTag::OBJECT);
 	gameObject->SetName("Item");
 
-	gameObject->AddComponent(new SphereCollider(0.5f));
+	gameObject->AddComponent(new SphereCollider(0.3f));
 	
 	std::string strPath(_tInvenItem->imgPath);
 	SpriteRenderer* sr = new SpriteRenderer(D3D9DEVICE->GetDevice(), ASSETMANAGER->GetTextureData(strPath +"0.png"));
@@ -30,11 +30,9 @@ void Item::Start(void) noexcept
 
 	for (int i = 0; i < 17; i++)
 	{
-		char str[256];
-		strPath += ("% d.png", i);
-		sprintf_s(str, 256, strPath.c_str());
+		std::string aniPath = strPath + std::to_string(i) + ".png";
 
-		TList.push_back(ASSETMANAGER->GetTextureData(str));
+		TList.push_back(ASSETMANAGER->GetTextureData(aniPath));
 		FrameTime.push_back(0.05f);
 	}
 
@@ -44,7 +42,7 @@ void Item::Start(void) noexcept
 
 	D3DXVECTOR3 pos;
 	pos.x = CE_MATH::Random(_vPlayerPos.x - 1, _vPlayerPos.x + 1);
-	pos.y =3;
+	pos.y = 1;
 	pos.z = CE_MATH::Random(_vPlayerPos.z - 1, _vPlayerPos.z + 1);
 
 	GetTransform()->SetWorldPosition(pos);
